c9e1223a6d645985f429a21bded2122d3ed8deb6
masroor <masroor.1388@gmail.com>
2024-02-27 22:54:11 +0330

    Conditional Rendering و form

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -3,39 +3,35 @@ import { taskContext } from './taskContext';
 
 const TaskItems = ()=>{
     const {taskItem,setTaskItem}=useContext(taskContext);
-    return (
-        <ul className="list-group m-0 p-0 mt-2">
+            if (taskItem.length){
+            return (
+                <ul className="list-group m-0 p-0 mt-2">
+                {
+                    taskItem.map(t=>
+                        <li className={
+                            `list-group-item d-flex justify-content-between 
+                            ${t.done? "list-group-item-success":""}`
+                        }>
+                            {t.title}
+                            <span>
+                            {
+                                t.done ? (
+                                    <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
+                                ):(
+                                    <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
+                                )
+                            }             
+                                <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
+                            </span>
+                        </li>
+                    )
+                }
+                </ul>    
+            )}else{
+               return <h4 className='text-center text-danger mt-4 '>هیچ کاری ثبت نشده است..</h4>
 
-        {
-            taskItem.map(t=>
-                <li className={
-                    `list-group-item d-flex justify-content-between 
-                    ${t.done? "list-group-item-success":""}`
-                }>
-                    {t.title}
-                    <span>
-                    {
-                        t.done ? (
-                            <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                        ):(
-                            <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                        )
-                    }
-                       
-                        <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-                    </span>
-                </li>
 
-
-            )
-        }
-
-
-
-       
-
-        </ul>    
-    )
+            }
 }
 
 export default TaskItems;
--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { taskContext } from './taskContext';
 
 const TopForm = ()=>{
+    const [task,setTask]=useState("")
+    const {taskItem,setTaskItem}=useContext(taskContext)
+
+    const handelSetTask=(e)=>{
+        setTask(e.target.value)
+    }
+
+    const handelAddTask=(e)=>{
+        e.preventDefault()
+        setTaskItem([...taskItem,{id:Math.random(),title:task,done:false}])
+    }
+
     return(
         <>
             <h4 className="text-center text-info text_shdow">به پروژه todo خوش آمدید</h4>
             <form>
-                <div className="form-group d-flex">
-                    <input type="text" className="form-control" />
-                    <button type="submit" className="btn btn-success me-1">ثبت</button>
+                <div className="form-group d-flex" >
+                    <input type="text" className="form-control" value={task}
+                    onChange={handelSetTask}/>
+                    <button type="submit" className="btn btn-success me-1"
+                    onClick={handelAddTask}>ثبت</button>
                 </div>
             </form>
         </>
